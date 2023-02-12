ac8ef733bfb4ee2c5c948f0fd5ed2e4935edddee
imtiajul-islam-d <imtiajul@hotmail.com>
2023-02-12 22:03:45 +0600

    sorting on cart component

--- src/pages/Main/Cart.js
+++ src/pages/Main/Cart.js
@@ -5,8 +5,10 @@ import ProductCard from "../../components/ProductCard";
 const Cart = () => {
   const cart = useSelector((state) => state.product.cart);
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      {cart.sort((a, b) => a._id - b._id).map((product) => (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
+      {cart
+        .sort((a, b) => a.cartPosition - b.cartPosition)
+        .map((product) => (
           <ProductCard key={product.model} product={product} />
         ))}
     </div>
